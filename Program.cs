7afe013cb6dfd2869f4843ea667362e884163ea7
Petr Starostin <starostin.petr@yandex.ru>
2023-03-26 20:49:06 +0400

    Second task

--- Program.cs
+++ Program.cs
@@ -30,3 +30,37 @@
 // int[] array = CreateRandomArray(size);
 // WriteArray(array);
 // Console.WriteLine($"Четных чисел в массиве: {VolumeOfEvenNumber(array)}");
+
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+// int SumOfOddIndexNum(int[] array)
+// {
+//     int result = 0;
+//     for(int i = 0 ; i < array.Length ; i++)
+//     {
+//         if(i % 2 != 0) result += array[i];
+//     }
+//     return result;
+// }
+
+// int [] CreateRandomArray(int size)
+// {
+//     int[] array = new int[size];
+
+//     for(int i = 0;  i < size; i++)
+//         array[i] = new Random().Next(1, 999 + 1);
+    
+//     return array;
+// }
+
+// void WriteArray(int[] array)
+// {
+//     for(int i = 0 ; i < array.Length; i++)
+//         Console.Write(array[i] + " ");
+    
+//     Console.WriteLine();
+// }
+// Console.Write("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateRandomArray(size);
+// WriteArray(array);
+// Console.WriteLine($"Сумма элементов на нечетных позициях: {SumOfOddIndexNum(array)}");
