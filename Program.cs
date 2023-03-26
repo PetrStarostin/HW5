925c8e95c8ce6c50dc792816d040df3f0d1e9d3b
Petr Starostin <starostin.petr@yandex.ru>
2023-03-26 20:36:16 +0400

    First task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// int VolumeOfEvenNumber(int[] array)
+// {
+//     int result = 0;
+//     for(int i = 0 ; i < array.Length ; i++)
+//     {
+//         if(array[i]% 2 == 0) result++;
+//     }
+//     return result;
+// }
+// int [] CreateRandomArray(int size)
+// {
+//     int[] array = new int[size];
+
+//     for(int i = 0;  i < size; i++)
+//         array[i] = new Random().Next(100, 999 + 1);
+    
+//     return array;
+// }
+
+// void WriteArray(int[] array)
+// {
+//     for(int i = 0 ; i < array.Length; i++)
+//         Console.Write(array[i] + " ");
+    
+//     Console.WriteLine();
+// }
+// Console.Write("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateRandomArray(size);
+// WriteArray(array);
+// Console.WriteLine($"Четных чисел в массиве: {VolumeOfEvenNumber(array)}");
